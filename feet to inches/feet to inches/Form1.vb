9a78d794633ee45ad28cd97058599dd2647c8498
gositonyourbed <115512014+gositonyourbed@users.noreply.github.com>
2022-10-10 17:39:27 -0700

    Add files via upload

---
+++ feet to inches/feet to inches/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Dim inches As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        inches = TextBox1.Text
+        Label2.Text = "Feet: " & Math.Round(inches / 12, 2)
+    End Sub
+End Class
